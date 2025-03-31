3f89cf72fbfc06eb76bd7a1127f396501315f7a5
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:23:32 +0200

    ensured user removes tartget on his own quiz

--- controllers/quiz/quiz.controller.js
+++ controllers/quiz/quiz.controller.js
@@ -767,7 +767,8 @@ router.delete('/:id/target', async (req, res) => {
 
     // check if quiz exist
     let quiz = await findDocument(Quiz, {
-      _id: req.params.id
+      _id: req.params.id,
+      user: req.user._id
     }, u, false)
     if (!quiz)
       return res.send(formatResult(404, 'quiz not found'))
