514e49645866bcf1655704003eebd52076b06afe
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:58:30 +0200

    updated admin.middleware.js

--- middlewares/admin.middleware.js
+++ middlewares/admin.middleware.js
@@ -1,5 +1,7 @@
+const {formatResult} = require("../utils/imports");
+
 function admin(req, res, next) {
-  if (req.user.category.name !== 'ADMIN') return res.send('You Have no access ...').status(403)
+  if (req.user.category.name !== 'ADMIN') return res.send(formatResult(403,'You Have no access ...'))
   next()
 }
 module.exports.admin = admin
