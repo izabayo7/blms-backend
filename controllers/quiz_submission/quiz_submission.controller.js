fce7455e5a3493ea5612bf6471dc918e6e3612ca
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:47:04 +0200

    fix: using answer to full fill last commit

--- controllers/quiz_submission/quiz_submission.controller.js
+++ controllers/quiz_submission/quiz_submission.controller.js
@@ -789,7 +789,7 @@ function validateSubmittedAnswers(questions, answers, mode) {
       }
       i = parseInt(i)
       // validate questions if it was not auto_submitted
-      if (!questions[i].auto_submitted) {
+      if (!answers[i].auto_submitted) {
         if (questions[i].type.includes('select')) {
           if (!answers[i].choosed_options) {
             message = `answer ${i + 1} must have choosed_options`
