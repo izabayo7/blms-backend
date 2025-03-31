ae55862c440f68285c2ed8fa505191471861bbc4
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 13:57:40 +0200

    bug fixed uploading images in content payload limit increased

--- index.js
+++ index.js
@@ -73,8 +73,14 @@ const userController = require('./controllers/user/user.controller')
 
 // use middlewares
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({
+// app.use(express.json());
+// app.use(express.urlencoded({
+//     extended: true
+// }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ 
+    limit: '50mb',
     extended: true
 }));
 
