5a9abd27e5dbd854dee95f431c62db4d9e1ae883
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:10:33 +0200

    added joi to reset_password.model

--- models/reset_password/reset_password.model.js
+++ models/reset_password/reset_password.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Joi } = require('../../utils/imports');
 const { PasswordRegex } = require('../user/user.model');
 
 const resetPassword = mongoose.Schema({
