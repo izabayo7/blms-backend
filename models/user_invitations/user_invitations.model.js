a2499cfdb2b705b3d26d54198558fdf0da316f4c
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:18:59 +0200

    added user_invitation model

---
+++ models/user_invitations/user_invitations.model.js
@@ -0,0 +1,56 @@
+// import dependencies
+const {
+    mongoose,
+    Joi,
+    timestamps
+} = require('../../utils/imports')
+
+const user_invitation_schema = new mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    college: {
+        type: mongoose.Types.ObjectId,
+        ref: "college",
+        required: true
+    },
+    category: {
+        type: mongoose.Types.ObjectId,
+        ref: "user_category",
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    expiration_date: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['PENDING', 'ACCEPTED', 'DENIED', 'EXPIRED'],
+        default: 0,
+
+    },
+}, { timestamps: true })
+
+// validate user_invitation
+exports.validate_user_invitation = (credentials) => {
+    const schema = {
+        email: Joi.string().email().required(),
+        user: Joi.ObjectId(),
+        college: Joi.ObjectId(),
+        category: Joi.ObjectId().required()
+    }
+    return Joi.validate(credentials, schema)
+}
+
+// create users model
+exports.user_invitation = mongoose.model('user_invitation', user_invitation_schema)
