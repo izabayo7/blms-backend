7f2c02f514b3e4c0ed24cad56dd2db26089edcbe
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:53:43 +0200

    added route for api to create request_callback email

--- routes/user_feedbacks/user_feedbacks.routes.js
+++ routes/user_feedbacks/user_feedbacks.routes.js
@@ -33,5 +33,39 @@ router.route('/contact_us')
      */
     .post(contactUs)
 
+router.route('/request_call_back')
+    /**
+     * @swagger
+     * /feedback/request_call_back:
+     *   post:
+     *     tags:
+     *       - User_feedback
+     *     description: Sends a call back request email to the communication team
+     *     parameters:
+     *       - name: body
+     *         description: Request callback fields
+     *         in: body
+     *         required: true
+     *         schema:
+     *           properties:
+     *             user_name:
+     *               type: string
+     *             institution_name:
+     *               type: string
+     *             role_at_institution:
+     *               type: string
+     *               enum:['INSTRUCTOR']
+     *             phone_number:
+     *               type: string
+     *           required:
+     *             - user_name
+     *             - institution_name
+     *             - institution_name
+     *             - phone_number
+     *     responses:
+     *       200:
+     *         description: Success
+     */
+    .post(requestCallback)
 
 exports.User_feedback_routes = router
