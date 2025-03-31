1c1e99b5d0549a2be9826305c05131d1617b3870
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:33:23 +0200

    fixed bug now sending group code on group creation instead of sending name

--- controllers/chat_group/chat_group.controller.js
+++ controllers/chat_group/chat_group.controller.js
@@ -494,7 +494,7 @@ router.post('/', async (req, res) => {
     })
 
     // ndanareba uko nahita menyesha abantu thinking about exporting socket for global usage
-    await Create_or_update_message('SYSTEM', result.data.name, `This group was created by __user__${creator._id} at __time__${new Date().toISOString()}`, u, creator._id)
+    const m = await Create_or_update_message('SYSTEM', result.data.code, `This group was created by __user__${creator._id} at __time__${new Date().toISOString()}`, u, creator._id)
 
     return res.send(result)
   } catch (error) {
