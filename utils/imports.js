04c22dcfd3de2ecf17de6b73969583afd8b274a2
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 15:58:38 +0200

    added real id

--- utils/imports.js
+++ utils/imports.js
@@ -518,6 +518,7 @@ module.exports.formatContacts = async (messages, user_id) => {
   let formatedContacts = []
   for (const message of messages) {
     let id = '',
+      _id = '',
       name = "",
       image = '',
       is_group = undefined,
@@ -556,6 +557,7 @@ module.exports.formatContacts = async (messages, user_id) => {
 
       id = group.name
       is_group = true
+      _id = group._id
       name = group.name
       members = await this.injectUser(group.members.filter(member => member.status == true), 'id', 'data')
       image = group.profile ? `http://${process.env.HOST}${process.env.BASE_PATH}/chat_group/${group._id}/profile/${group.profile}` : undefined
@@ -589,6 +591,7 @@ module.exports.formatContacts = async (messages, user_id) => {
     }
     formatedContacts.push({
       id: id,
+      _id: _id,
       name: name,
       image: image,
       last_message: last_message,
