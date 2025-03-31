46524becb0698692fa3f9ecc39ced81a8355beda
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 11:44:15 +0200

    added route to api to get all invitations

--- routes/user_invitations/user_invitations.routes.js
+++ routes/user_invitations/user_invitations.routes.js
@@ -68,3 +68,28 @@ router.route('/')
      */
     .post([auth, createUserInvitation])
 
+    router.route('/all')
+    /**
+     * @swagger
+     * /user_invitations/all:
+     *   get:
+     *     tags:
+     *       - User_invitation
+     *     description: Returns all user_invitations
+     *     parameters:
+     *       - name: page
+     *         description: page number
+     *         in: query
+     *         type: string
+     *       - name: limit
+     *         description: elements per page
+     *         in: query
+     *         type: string
+     *     responses:
+     *       200:
+     *         description: Success
+     *       500:
+     *         description: Internal Server Error
+     */
+    .get([auth, getAllInvitations])
+
