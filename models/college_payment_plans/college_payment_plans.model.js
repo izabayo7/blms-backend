a9a0c0e027cf32bf88fa34cd2756037878acf305
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 17:42:59 +0200

    added College_payment_plans swagger definition

--- models/college_payment_plans/college_payment_plans.model.js
+++ models/college_payment_plans/college_payment_plans.model.js
@@ -4,6 +4,28 @@ const {
     Joi,
     timestamps
 } = require('../../utils/imports')
+
+/**
+ * @swagger
+ * definitions:
+ *   College_payment_plans:
+ *     properties:
+ *       college:
+ *         type: string
+ *       plan:
+ *         type: string
+ *         enum: ['TRIAL', 'HUGUKA', 'JIJUKA', 'MINUZA']
+ *       discount:
+ *         type: number
+ *         default: 20
+ *       status:
+ *         type: string
+ *         enum: ['ACTIVE','INACTIVE']
+ *     required:
+ *       - name
+ *       - email
+ */
+
 const plans = ['TRIAL', 'HUGUKA', 'JIJUKA', 'MINUZA']
 
 const schema = new mongoose.Schema({
@@ -21,8 +43,9 @@ const schema = new mongoose.Schema({
         default: 20
     },
     status: {
-        type: Number,
-        default: 1
+        type: String,
+        enum: ['ACTIVE','INACTIVE'],
+        default: 'ACTIVE'
     },
 }, {timestamps: true})
 
@@ -37,6 +60,6 @@ function validate_college_payment_plans(credentials) {
 }
 
 // export the model and the validation function
-module.exports.college_payment_plans = mongoose.model('college_payment_plans', schema)
+module.exports.College_payment_plans = mongoose.model('college_payment_plans', schema)
 module.exports.validate_college_payment_plans= validate_college_payment_plans
 
