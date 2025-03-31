d21afcb933c8578adc52f7eb104eb886ed0b1e0a
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:25:23 +0200

    added email to notify users when marks are released

--- utils/emailGenerator.js
+++ utils/emailGenerator.js
@@ -326,6 +326,195 @@ exports.submission_email = ({
     return result;
 };
 
+exports.marks_release_email = ({
+                                   user_names,
+                                   instructor_names,
+                                   assignment_name,
+                                   assignment_type,
+                                   link,
+                               }) => {
+    const result = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width" />
+            <style>
+                @import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
+            </style>
+        </head>
+        
+        <body style="margin: 0; background-color: #ececec">
+        <div class="logo" style="text-align: center; margin: 25px auto">
+            <a href="https://kurious.rw"><img src="https://apis.kurious.rw/assets/images/logo.png" alt="" /></a>
+        </div>
+        <div class="flex">
+            <div class="">
+                <div
+                        class="content"
+                        style="
+                                background-color: white;
+                                border-top: 3px solid #193074;
+                                padding: 69px 39px 0;
+                                margin: auto;
+                                max-width: 522px;
+                                border-radius: 0px;
+                                align-self: center;
+                              "
+                >
+                    <div
+                            class="message"
+                            style="
+                                  font-family: 'Inter', sans-serif;
+                                  font-style: normal;
+                                  font-weight: normal;
+                                  font-size: 15px;
+                                  line-height: 24px;
+                                  color: #343434;
+                                  width: 379px;
+                                  left: 139px;
+                                  top: 126px;
+                                  margin-bottom: 10px;
+                                "
+                    >
+                        Hi ${user_names}. <br>
+                        Instructor ${instructor_names} released marks for ${assignment_name} ${assignment_type}. <br>
+                        Click the button below to view your results. <br>
+        
+                    </div>
+                    <div class="text-center" style="text-align: center">
+                        <a
+                                href="${link}"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                        >
+                            <button
+                                    class="confirm"
+                                    style="
+                                      height: 54px;
+                                      width: 172px;
+                                      border-radius: 10px;
+                                      background: #193074;
+                                      color: white;
+                                      border: none;
+                                      text-align: center;
+                                      margin: 31px auto 4px;
+                                      cursor: pointer;
+                                    "
+                                    onfocus="this.style.outline='none'"
+                            >
+                                VIEW RESULTS
+                            </button>
+                        </a>
+                    </div>
+                    <div
+                            class="thanks"
+                            style="
+                                  font-family: 'Inter', sans-serif;
+                                  font-size: 15px;
+                                  font-style: normal;
+                                  font-weight: 400;
+                                  line-height: 24px;
+                                  letter-spacing: 0em;
+                                  text-align: left;
+                                  margin: 50px 0 0;
+                                  height: 60px;
+                                  width: 126px;
+                                  left: 39px;
+                                  top: 328px;
+                                "
+                    >
+                        Thanks,
+                        <br />
+                        Kurious learn team
+                    </div>
+                    <div
+                            style="
+                                  padding: 12px;
+                                  text-align: center;
+                                  font-family: 'Helvetica';
+                                  font-style: normal;
+                                  font-weight: 300;
+                                  font-size: 10px;
+                                  line-height: 30px;
+                                  color: #343434;
+                                "
+                    >
+                        Learn, Connect, Engage
+                    </div>
+                </div>
+                <div
+                        class="footer"
+                        style="
+                                height: 66px;
+                                left: 0px;
+                                top: 476px;
+                                border-radius: 0px;
+                                max-width: 600px;
+                                margin: auto;
+                                background: #193074;
+                              "
+                >
+                    <a
+                            href="#"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                    >
+                        <img
+                                class="ig_icon"
+                                style="
+                                    height: 20px;
+                                    width: 20px;
+                                    left: 41px;
+                                    top: 499px;
+                                    border-radius: 0px;
+                                    margin: 24px 12px;
+                                  "
+                                src="https://apis.kurious.rw/assets/images/ig.png"
+                                alt=""
+                        />
+                    </a>
+                    <a
+                            href="https://kurious.rw"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                    >
+                        <img
+                                class="fb_icon"
+                                style="
+                                    height: 20px;
+                                    width: 11px;
+                                    left: 87px;
+                                    top: 499px;
+                                    border-radius: 0px;
+                                    margin: 24px 12px;
+                                  "
+                                src="https://apis.kurious.rw/assets/images/fb.png"
+                                alt=""
+                        />
+                    </a>
+                    <a
+                            href="https://twitter.com/kuriouslearnRw"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                    >
+                        <img
+                                class="twitter_icon"
+                                style="left: 87px; margin: 24px 12px"
+                                src="https://apis.kurious.rw/assets/images/twitter.png"
+                                alt=""
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>
+        </body>
+        </html>
+  `;
+
+    return result;
+};
+
 exports.confirmation_email = ({
                                   user_name,
                                   institution_name,
@@ -602,9 +791,9 @@ exports.confirmation_email = ({
 };
 
 exports.confirm_email = ({
-                               user_name,
-                               token
-                           }) => {
+                             user_name,
+                             token
+                         }) => {
     const result = `
 <!DOCTYPE html>
 <html lang="en">
