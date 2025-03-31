00dca6bae0d53cb9226bfc276ce816256c507c9c
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 13:40:26 +0200

    updated student.middleware.js

--- middlewares/student.middleware.js
+++ middlewares/student.middleware.js
@@ -1,5 +1,7 @@
+const {formatResult} = require("../utils/imports");
+
 function student(req, res, next) {
-  if (!req.user.category === 'Student') return res.send('You Have no access ...').status(403)
+  if (req.user.category.name === 'STUDENT') return res.send(formatResult(403, 'You Have no access ...')).status(403)
   next()
 }
 module.exports.student = student
--- middlewares/superAdmin.middleware.js
+++ middlewares/superAdmin.middleware.js
@@ -1,7 +1,7 @@
 const {formatResult} = require("../utils/imports");
 
 function superAdmin(req, res, next) {
-  if (req.user.category.name !== 'SuperAdmin') return res.send(formatResult(403, "You have no access ..."))
+  if (req.user.category.name !== 'SUPERADMIN') return res.send(formatResult(403, "You have no access ..."))
   next()
 }
 module.exports.superAdmin = superAdmin
