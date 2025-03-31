47e0e54e67f35537ee9688de031c562149906070
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 16:51:55 +0200

    change now using use_name when creating a course to get user_id

--- controllers/course/course.controller.js
+++ controllers/course/course.controller.js
@@ -488,7 +488,7 @@ router.post('/', async (req, res) => {
         })
 
         let user = await findDocument(User, {
-            _id: req.body.user
+            user_name: req.body.user_name
         })
         if (!user)
             return res.send(formatResult(404, 'user not found'))
