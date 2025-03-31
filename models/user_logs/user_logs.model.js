451bc6d66f80a8751fe5cae30c7f1ead5cfef38a
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 14:50:41 +0200

    added user logs model

--- models/user_logs/user_logs.model.js
+++ models/user_logs/user_logs.model.js
@@ -5,7 +5,6 @@ const {
     timestamps,
 } = require('../../utils/imports')
 
-const status_enum = ['ACTIVE', 'INACTIVE']
 const paginate = require('mongoose-paginate-v2')
 
 const dailyLogSchema = new mongoose.Schema({
@@ -13,20 +12,21 @@ const dailyLogSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-    accessed_course: {
-        type: Number,
-        default: 0
-    },
-    accessed_live_stream: {
-        type: Number,
-        default: 0
-    },
+    accessed_course: [{
+        type: String,
+        ref: 'course',
+    }],
+    accessed_live_stream: [{
+        type: String,
+        ref: 'live_session',
+    }],
 },{timestamps: true})
 
 const user_log_schema = new mongoose.Schema({
     user: {
         type: String,
         ref: 'user',
+        unique: true,
         required: true
     },
     // handle online status, course access, liveclass attendance for user every day
@@ -38,5 +38,16 @@ user_log_schema.plugin(paginate)
 // create user_user_group model
 const user_log = mongoose.model('user_log', user_log_schema)
 
+// validate user_user_group
+function validate_user_log(credentials) {
+    const schema = {
+        online: Joi.boolean(),
+        course_id: Joi.ObjectId(),
+        live_session_id: Joi.ObjectId(),
+    }
+    return Joi.validate(credentials, schema)
+}
+
 // export the model and the validation function
 module.exports.User_logs = user_log
+module.exports.validate_user_log = validate_user_log
