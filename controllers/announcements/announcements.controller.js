56025789b6dad1ec34963c44f9be49faa5a1f28b
izabayo7 <cedricizabayo7@gmail.com>
2025-03-31 10:17:24 +0200

    added api to delete announcements

---
+++ controllers/announcements/announcements.controller.js
@@ -0,0 +1,312 @@
+// import dependencies
+const {filterUsers} = require("../../middlewares/auth.middleware");
+const {
+  express,
+  Announcement,
+  validate_announcement,
+  validateObjectId,
+  formatResult,
+  findDocument,
+  User,
+  findDocuments,
+  u,
+  Create_or_update_announcement,
+  deleteDocument,
+  Live_session,
+  createDocument,
+  updateDocument,
+  Chapter,
+  injectUser,
+  injectAnnouncementsReplys,
+  simplifyObject,
+  Quiz_submission
+} = require('../../utils/imports')
+
+// create router
+const router = express.Router()
+
+/**
+ * @swagger
+ * /announcement/{target}/{id}:
+ *   get:
+ *     tags:
+ *       - Announcement
+ *     description: Returns announcements in the specified target
+ *     security:
+ *       - bearerAuth: -[]
+ *     parameters:
+ *       - name: target
+ *         description: target type
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: id
+ *         description: target id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal Server error
+ */
+router.get('/:target/:id', async (req, res) => {
+  try {
+
+    let {
+      error
+    } = validateObjectId(req.params.id)
+    if (error)
+      return res.send(formatResult(400, error.details[0].message))
+
+    const allowedTargetTypes = ['chapter', 'live_session', 'quiz_submission', 'quiz_submission_answer']
+
+    if (!allowedTargetTypes.includes(req.params.target)) {
+      return res.send(formatResult(400, 'invalid target type'))
+    }
+
+    let target
+
+    switch (req.params.target) {
+      case 'chapter':
+        target = await findDocument(Chapter, {
+          _id: req.params.id
+        })
+        break;
+
+      case 'live_session':
+        target = await findDocument(Live_session, {
+          _id: req.params.id
+        })
+        break;
+
+      case 'quiz_submission':
+        target = await findDocument(Quiz_submission, {
+          _id: req.params.id
+        })
+        break;
+
+      case 'quiz_submission_answer':
+        target = await findDocument(Quiz_submission, {
+          "answers._id": req.params.id
+        })
+        break;
+
+      default:
+        break;
+    }
+
+    if (!target)
+      return res.send(formatResult(404, 'announcement target not found'))
+
+    let announcements = await findDocuments(Announcement, {
+      "target.type": req.params.target,
+      "target.id": req.params.id,
+      reply: undefined
+    }, u, u, u, u, u, { _id: -1 })
+    announcements = await injectUser(announcements, 'sender')
+    announcements = await injectAnnouncementsReplys(announcements)
+    return res.send(formatResult(u, u, announcements))
+  } catch (error) {
+    return res.send(formatResult(500, error))
+  }
+})
+
+/**
+ * @swagger
+ * /announcement:
+ *   post:
+ *     tags:
+ *       - Announcement
+ *     description: Send a announcement
+ *     security:
+ *       - bearerAuth: -[]
+ *     parameters:
+ *       - name: body
+ *         description: Fields for a announcement
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Announcement'
+ *     responses:
+ *       201:
+ *         description: Created
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal Server error
+ */
+router.post('/', async (req, res) => {
+  try {
+    const {
+      error
+    } = validate_announcement(req.body)
+    if (error)
+      return res.send(formatResult(400, error.details[0].message))
+
+    const user = await findDocument(User, { user_name: req.body.sender })
+    if (!user)
+      return res.send(formatResult(404, 'user not found'))
+
+    req.body.sender = user._id
+
+    req.body.target.type = req.body.target.type.toLowerCase()
+
+    const allowedTargets = ['chapter', 'live_session', 'quiz_submission', 'quiz_submission_answer']
+
+    if (!allowedTargets.includes(req.body.target.type))
+      return res.send(formatResult(400, 'invalid announcement target_type'))
+
+    let target
+
+    switch (req.body.target.type) {
+      case 'chapter':
+        target = await findDocument(Chapter, {
+          _id: req.body.target.id
+        })
+        break;
+
+      case 'live_session':
+        target = await findDocument(Live_session, {
+          _id: req.body.target.id
+        })
+        break;
+
+      case 'quiz_submission':
+        target = await findDocument(Quiz_submission, {
+          _id: req.body.target.id
+        })
+        break;
+
+      case 'quiz_submission_answer':
+        target = await findDocument(Quiz_submission, {
+          "answers._id": req.body.target.id
+        })
+        break;
+
+      default:
+        break;
+    }
+
+    if (!target)
+      return res.send(formatResult(404, 'announcement target not found'))
+
+
+    let result = await createDocument(Announcement, req.body)
+    result = simplifyObject(result)
+    result.data = await injectUser([result.data], 'sender')
+    result.data = result.data[0]
+
+    return res.send(result)
+  } catch (error) {
+    return res.send(formatResult(500, error))
+  }
+})
+
+/**
+ * @swagger
+ * /announcement/{id}:
+ *   put:
+ *     tags:
+ *       - Announcement
+ *     description: Update a announcement
+ *     security:
+ *       - bearerAuth: -[]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         type: string
+ *         description: Announcement's Id
+ *       - name: body
+ *         description: Fields for a Announcement
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Announcement'
+ *     responses:
+ *       201:
+ *         description: Created
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal Server error
+ */
+router.put('/:id', async (req, res) => {
+  try {
+    let {
+      error
+    } = validateObjectId(req.params.id)
+    if (error)
+      return res.send(formatResult(400, error.details[0].message))
+    error = validate_announcement(req.body, 'update')
+    error = error.error
+    if (error)
+      return res.send(formatResult(400, error.details[0].message))
+
+    const announcement = await findDocument(Announcement, { _id: req.params.id })
+    if (!announcement)
+      return res.send(formatResult(404, 'announcement not found'))
+
+    const result = await updateDocument(Announcement, req.params.id, req.body)
+    return res.send(result)
+  } catch (error) {
+    return res.send(formatResult(500, error))
+  }
+})
+
+/**
+ * @swagger
+ * /announcement/{id}:
+ *   delete:
+ *     tags:
+ *       - Announcement
+ *     description: Delete announcement
+ *     security:
+ *       - bearerAuth: -[]
+ *     parameters:
+ *       - name: id
+ *         description: Announcement's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal Server error
+ */
+router.delete('/:id', filterUsers(["ADMIN","INSTRUCTOR"]), async (req, res) => {
+  try {
+    const {
+      error
+    } = validateObjectId(req.params.id)
+    if (error)
+      return res.send(formatResult(400, error.details[0].message))
+
+    const announcement = await findDocument(Announcement, {
+      _id: req.params.id
+    })
+    if (!announcement)
+      return res.send(formatResult(404, 'announcement not found'))
+
+    const result = await deleteDocument(Announcement, req.params.id)
+
+    return res.send(result)
+  } catch (error) {
+    return res.send(formatResult(500, error))
+  }
+})
+
+// export the router
+module.exports = router
